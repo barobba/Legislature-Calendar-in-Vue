f5658f3f060ace99de4b377496a303efb21c38b0
Biagio Arobba <barobba@gmail.com>
2022-05-28 15:49:40 -0500

    Saved for sharing changes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// import Bugsnag from "@bugsnag/js";
+// import BugsnagPluginVue from "@bugsnag/plugin-vue";
+// Bugsnag.start({
+//   apiKey: process.env.BUGSNAG_API_KEY,
+//   plugins: [new BugsnagPluginVue()],
+// });
 
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
